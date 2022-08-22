e3eea98b3dd9a17a3f2abf7b303379fde94bc464
ikhvjs <68546412+ikhvjs@users.noreply.github.com>
2022-08-22 17:05:13 +0200

    add DB configuration

---
+++ src/main/java/github/ikhvjs/recipes/model/Recipe.java
@@ -0,0 +1,32 @@
+package github.ikhvjs.recipes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String recipe_name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipe_name() {
+        return recipe_name;
+    }
+
+    public void setRecipe_name(String recipe_name) {
+        this.recipe_name = recipe_name;
+    }
+
+
+}
