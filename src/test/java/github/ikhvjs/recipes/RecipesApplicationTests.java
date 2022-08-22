555e0fccedff13c641002bff9a443b6430fcf09d
ikhvjs <68546412+ikhvjs@users.noreply.github.com>
2022-08-22 15:04:06 +0200

    add project skeleton

---
+++ src/test/java/github/ikhvjs/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package github.ikhvjs.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
